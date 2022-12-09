2b1f2afda996a51eaed96aaa276bb2036612398b
Chikatanaka <ume12.vmd38@gmail.com>
2022-12-09 12:30:13 +0000

    課題　laravel06

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
+@extends('layouts.profile')
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>Myニュース作成画面</h1>
-    </body>
-</html>
+@section('title','プロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
